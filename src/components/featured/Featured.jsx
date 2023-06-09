c44742221385bc7ec918ed45b3501d0f55d98032
adrianpychan <adrianpychann@gmail.com>
2023-06-10 00:53:36 +0800

    first draft consisting of navbar and featured movie, lists to be completed

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,53 @@
+import "./featured.scss";
+import { PlayArrow } from '@material-ui/icons';
+import { InfoOutlined } from '@material-ui/icons';
+
+export default function Featured({type}) {
+    //if they see a type (series, movies, they will see the following....)
+    // If type parameter = movie, then Movies will be displayed, otherwise it will show Series.
+  return (
+    <div className="featured">
+        {type && (
+            <div className="category">
+                <span>{type === "movie" ? "Movies" : "Series"}</span>
+                <select name="genre" id="genre">
+                    <option>Genre</option>
+                    <option value="adventure">Adventure</option>
+                    <option value="comedy">Comedy</option>
+                    <option value="crime">Crime</option>
+                    <option value="fantasy">Fantasy</option>
+                    <option value="historical">Historical</option>
+                    <option value="horror">Horror</option>
+                    <option value="romance">Romance</option>
+                    <option value="sci-fi">Sci-fi</option>
+                    <option value="thriller">Thriller</option>
+                    <option value="western">Western</option>
+                    <option value="animation">Animation</option>
+                    <option value="drama">Drama</option>
+                    <option value="documentary">Documentary</option>
+                </select>
+            </div>
+        )}
+        <img 
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+            alt="" 
+        />
+        <div className="info">
+            <img src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1" alt="" />
+            <span className="desc">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </span>
+            <span className="buttons">
+                <button className="play">
+                    <PlayArrow />
+                    <span>Play</span>
+                </button>
+                <button className="more">
+                    <InfoOutlined />
+                    <span>More</span>
+                </button>
+            </span>
+        </div>
+    </div>
+  )
+}
